3a9384ad634949b93afbbe0c6317268c01df108b
홍성민 <seanhong2000@gmail.com>
2023-05-27 05:08:40 +0900

    Initial Commit

---
+++ GNG-ios/GNG_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNG_iosApp.swift
+//  GNG-ios
+//
+//  Created by Sean Hong on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct GNG_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
